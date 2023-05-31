e0f06673b50b1a893ba79558b839d4fcc39ea84b
jeffzhux <jeffzhux@gmail.com>
2023-05-31 17:50:23 +0800

    2023.5.31 upload

--- AgriDefender/public/javascripts/image-loader.js
+++ AgriDefender/public/javascripts/image-loader.js
@@ -1,46 +1,49 @@
-/**
- * Loads an image from url
- */
-class ImageLoader {
-  constructor(imageWidth, imageHeight) {
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = imageWidth;
-    this.canvas.height = imageHeight;
-    this.ctx = this.canvas.getContext('2d');
-  }
-  async getImageData(url) {
-    await this.loadImageAsync(url);
-    const imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-    return imageData;
-  }
-  loadImageAsync(url) {
-    return new Promise((resolve, reject) => {
-      this.loadImageCb(url, () => {
-        resolve();
-      });
-    });
-  }
-  loadImageCb(url, cb) {
-    loadImage(
-      url,
-      img => {
-        if (img.type === 'error') {
-          throw `Could not load image: ${url}`;
-        } else {
-          // load image data onto input canvas
-          this.ctx.drawImage(img, 0, 0)
-          //console.log(`image was loaded`);
-          window.setTimeout(() => { cb(); }, 0);
-        }
-      },
-      {
-        maxWidth: this.canvas.width,
-        maxHeight: this.canvas.height,
-        cover: true,
-        crop: true,
-        canvas: true,
-        crossOrigin: 'Anonymous'
+function imagenetClassesTopK(classProbabilities, k) {
+  if (!k) { k = 5; }
+  const probs = Array.from(classProbabilities);
+  const probsIndices = probs.map(
+    function (prob, index) {
+      return [prob, index];
+    }
+  );
+  const sorted = probsIndices.sort(
+    function (a, b) {
+      if (a[0] < b[0]) {
+        return -1;
       }
-    );
-  }
+      if (a[0] > b[0]) {
+        return 1;
+      }
+      return 0;
+    }
+  ).reverse();
+  const topK = sorted.slice(0, k).map(function (probIndex) {
+      const iClass = $.index.var.classindex[probIndex[1]];
+      return {
+          id: iClass,
+          index: parseInt(probIndex[1], 10),
+          name: iClass.replace(/_/g, ' '),
+          probability: probIndex[0] * 100
+      };
+  });
+  return topK;
+}
+function preprocess(data, width, height) {
+  const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
+  const dataProcessed = ndarray(new Float32Array(width * height * 3), [1, 3, height, width]);
+
+  // Normalize 0-255 to (-1)-1
+  ndarray.ops.divseq(dataFromImage, 255.0);
+  
+  ndarray.ops.subseq(dataFromImage.pick(null, null, 2), 0.4914);
+  ndarray.ops.divseq(dataFromImage.pick(null, null, 2), 0.2471);
+  ndarray.ops.subseq(dataFromImage.pick(null, null, 1), 0.4822);
+  ndarray.ops.divseq(dataFromImage.pick(null, null, 1), 0.2435);
+  ndarray.ops.subseq(dataFromImage.pick(null, null, 0), 0.4465);
+  ndarray.ops.divseq(dataFromImage.pick(null, null, 0), 0.2616);
+
+  ndarray.ops.assign(dataProcessed.pick(0, 0, null, null), dataFromImage.pick(null, null, 2));
+  ndarray.ops.assign(dataProcessed.pick(0, 1, null, null), dataFromImage.pick(null, null, 1));
+  ndarray.ops.assign(dataProcessed.pick(0, 2, null, null), dataFromImage.pick(null, null, 0));
+  return dataProcessed.data;
 }
--- AgriDefender/public/javascripts/index.ui.js
+++ AgriDefender/public/javascripts/index.ui.js
@@ -42,6 +42,8 @@
                 
                 img.onload = function(){
                     var canvas = document.getElementById('canvas');
+                    // var canvas = document.createElement('canvas');
+
                     const ctx = canvas.getContext('2d');
                     var maxWidth = canvas.width;
                     var maxHeight = canvas.height;
@@ -56,6 +58,7 @@
                     var x = (canvas.width - width) / 2;
                     var y = (canvas.height - height) / 2;
                     ctx.drawImage(img, x, y, width, height);
+                    $("#dropArea").html(canvas);
                     $('#imagePreview').show();
                     recongnize();
                 }
@@ -64,10 +67,7 @@
             };
             reader.readAsDataURL(file);
 
-        
         },
-        
-
     })
     async function recongnize(){
         var canvas = document.getElementById('canvas');
@@ -77,7 +77,6 @@
         preprocessData = preprocess(imgData.data, canvas.width, canvas.height)
 
         const input = new onnx.Tensor(preprocessData, "float32", [1, 3,canvas.height, canvas.width]);
-        console.log(input)
         
         const outputMap = await $.index.var.sess.run([input]);
         const outputTensor = outputMap.values().next().value.data;
@@ -86,57 +85,11 @@
         const resultText = topk.map(p => `${p.name}: ${p.probability.toFixed(4)}`).join('<br>');
           $('#classification').html(resultText);
     }
-    function imagenetClassesTopK(classProbabilities, k) {
-        if (!k) { k = 5; }
-        const probs = Array.from(classProbabilities);
-        const probsIndices = probs.map(
-          function (prob, index) {
-            return [prob, index];
-          }
-        );
-        const sorted = probsIndices.sort(
-          function (a, b) {
-            if (a[0] < b[0]) {
-              return -1;
-            }
-            if (a[0] > b[0]) {
-              return 1;
-            }
-            return 0;
-          }
-        ).reverse();
-        const topK = sorted.slice(0, k).map(function (probIndex) {
-            const iClass = $.index.var.classindex[probIndex[1]];
-            return {
-                id: iClass,
-                index: parseInt(probIndex[1], 10),
-                name: iClass.replace(/_/g, ' '),
-                probability: probIndex[0] * 100
-            };
-        });
-        return topK;
-    }
-    function preprocess(data, width, height) {
-        const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
-        const dataProcessed = ndarray(new Float32Array(width * height * 3), [1, 3, height, width]);
-      
-        // Normalize 0-255 to (-1)-1
-        ndarray.ops.divseq(dataFromImage, 255.0);
-        ndarray.ops.subseq(dataFromImage, 0.5);
-        ndarray.ops.divseq(dataFromImage, 0.5);
-
-        // Realign imageData from [224*224*4] to the correct dimension [1*3*224*224].
-        ndarray.ops.assign(dataProcessed.pick(0, 0, null, null), dataFromImage.pick(null, null, 2));
-        ndarray.ops.assign(dataProcessed.pick(0, 1, null, null), dataFromImage.pick(null, null, 1));
-        ndarray.ops.assign(dataProcessed.pick(0, 2, null, null), dataFromImage.pick(null, null, 0));
-      
-        return dataProcessed.data;
-    }
     $(window).ready(function(){
         $.index.var.sess = new onnx.InferenceSession(),
-        $.index.var.sess.loadModel("../weights/two_expert.onnx").then(() =>{
+        $.index.var.sess.loadModel("../weights/sl.onnx").then(() =>{
             $.index.ui._init();
-            $('#dropArea').html("<p>Drag and drop an image here<br/>or click to select an image</p>");
+            //$('#dropArea').html("<p>Drag and drop an image here<br/>or click to select an image</p>");
         })
     });
 
